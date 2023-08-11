5a9c0bc5a5a05bc9ff8905edf2eb35a06e729fc0
rhmkstk <rahmi.kstk@gmail.com>
2023-08-11 16:49:20 +0300

    create hero section for demo day

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="max-w-2xl px-4 m-auto min-h-screen py-24">
+  <section class="max-w-2xl px-4 m-auto min-h-screen py-24 mt-4">
+    <div class="hero bg-[#26264B] py-10 px-4 text-white text-center rounded-md">
+      <img
+        src="https://yazilimcigunleri.com/wp-content/uploads/2019/10/logo.png"
+        alt="12 - 13 Ağustos Yazılımcı Günleri"
+        srcset="https://yazilimcigunleri.com/wp-content/uploads/2019/10/logo.png 3x"
+        class="mx-auto mb-8"
+      >
+      <h4 class="mb-2 text-2">
+        TÜKENMEDEN YERINI AL
+      </h4>
+      <h4 class="text-6xl font-semibold">
+        YAZILIMCI GUNLERI ETKINLIGI
+      </h4>
+      <div class="flex items-center space-x-4 mx-auto w-max mt-8">
+        <div class="flex items-center space-x-3">
+          <img
+            decoding="async"
+            src="https://yazilimcigunleri.com/wp-content/uploads/2019/11/earth-globe.svg"
+            alt="earth-globe"
+            width="23"
+            height="23"
+            data-no-retina=""
+            data-src-rs-ref="https://yazilimcigunleri.com/wp-content/uploads/2019/11/earth-globe.svg"
+          >
+          <span>IPA KAMPUS, FLORYA</span>
+        </div>
+        <div class="flex items-center space-x-3">
+          <img
+            decoding="async"
+            src="https://yazilimcigunleri.com/wp-content/uploads/2019/11/calendar.svg"
+            alt="earth-globe"
+            width="23"
+            height="23"
+            data-no-retina=""
+            data-src-rs-ref="https://yazilimcigunleri.com/wp-content/uploads/2019/11/earth-globe.svg"
+          >
+          <span>12,13 AGUSTOS 2023</span>
+        </div>
+      </div>
+      <a href="/test" class="px-10 py-2 bg-purple-500 hover:bg-purple-400 inline-block text-white rounded-sm text-xl mt-8">
+        Kayit ol
+      </a>
+    </div>
     <div v-if="logs && logs.length > 0" class="logs divide-y divide-gray-100">
       <LogCard v-for="log in logs" :key="log.ID" :log="log" />
     </div>
@@ -16,7 +59,7 @@
         Browse all updates <i class="ri-arrow-down-s-line text-2xl ml-2" />
       </button>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
